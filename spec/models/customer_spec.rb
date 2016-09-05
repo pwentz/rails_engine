8c686868c499ddb0eea479214566d0b68d517637
pwentz <patrick.w.wentz@gmail.com>
2016-09-05 14:23:51 -0600

    close #5 add favorite merchant query to customer model.

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,4 +6,30 @@ RSpec.describe Customer, type: :model do
     it { should have_many(:merchants).through(:invoices) }
     it { should have_many(:transactions).through(:invoices) }
   end
+
+  context 'instance methods' do
+    it "knows it's favorite merchant" do
+      customer = create(:customer)
+      merch_one,
+        merch_two = create_list(:merchant, 2)
+      invoices_one = create_list(
+        :invoice,
+        2,
+        customer: customer,
+        merchant: merch_one
+      )
+      invoices_two = create_list(
+        :invoice,
+        2,
+        customer: customer,
+        merchant: merch_two
+      )
+      create(:transaction, invoice: invoices_one.first, result: 'failed')
+      create(:transaction, invoice: invoices_one.last)
+      create(:transaction, invoice: invoices_two.first)
+      create(:transaction, invoice: invoices_two.last)
+
+      expect(customer.favorite_merchant).to eq(merch_two)
+    end
+  end
 end
