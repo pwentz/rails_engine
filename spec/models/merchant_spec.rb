1e879460d74218684f7203bdb30db008eaaa375a
pwentz <patrick.w.wentz@gmail.com>
2016-09-05 19:12:07 -0600

    fix hound

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -12,27 +12,36 @@ RSpec.describe Merchant, type: :model do
     merchant_two,
     merchant_three = create_list(:merchant, 3)
 
-    invoice_one = create( :invoice, merchant: merchant_one )
-    invoice_two = create( :invoice, merchant: merchant_two )
-    invoice_three = create( :invoice, merchant: merchant_three )
+    invoice_one = create(:invoice, merchant: merchant_one)
+    invoice_two = create(:invoice, merchant: merchant_two)
+    invoice_three = create(:invoice, merchant: merchant_three)
 
-    item_one = create( :item, merchant: merchant_one )
-    item_two = create( :item, merchant: merchant_two )
-    item_three = create( :item, merchant: merchant_three )
+    item_one = create(:item, merchant: merchant_one)
+    item_two = create(:item, merchant: merchant_two)
+    item_three = create(:item, merchant: merchant_three)
 
-    invoice_item_one = create( :invoice_item, quantity: 7,
-                               item: item_one,
-                               invoice: invoice_one )
-    invoice_item_two = create( :invoice_item, quantity: 2,
-                               item: item_two,
-                               invoice: invoice_two )
-    invoice_item_three = create( :invoice_item, quantity: 6,
-                                 item: item_three,
-                                 invoice: invoice_three )
+    invoice_item_one = create(
+      :invoice_item,
+      quantity: 7,
+      item: item_one,
+      invoice: invoice_one
+    )
+    invoice_item_two = create(
+      :invoice_item,
+      quantity: 2,
+      item: item_two,
+      invoice: invoice_two
+    )
+    invoice_item_three = create(
+      :invoice_item,
+      quantity: 6,
+      item: item_three,
+      invoice: invoice_three
+    )
 
 
     expect(
       Merchant.most_items_sold(2)
-    ).to eq([ merchant_one, merchant_three ])
+    ).to eq([merchant_one, merchant_three])
   end
 end
