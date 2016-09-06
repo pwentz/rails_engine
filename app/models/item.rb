15a29307ea6567a9af8177bafc596f792bfe57d0
pwentz <patrick.w.wentz@gmail.com>
2016-09-06 07:41:46 -0600

    close #23 returns top x items in order of total revenue

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,15 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def self.most_revenue(quantity)
+    Item.select(
+      "items.*, "\
+      "SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue"
+      ).
+      joins(:invoice_items).
+      order("revenue DESC").
+      group("items.id").
+      first(quantity)
+  end
 end
