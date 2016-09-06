c32af0a4ac1d24f301bdd58528381c2275b75f3f
pwentz <patrick.w.wentz@gmail.com>
2016-09-06 14:41:53 -0600

    adjusted routes file

--- app/controllers/api/v1/items/revenue_controller.rb
+++ app/controllers/api/v1/items/revenue_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::Items::RevenueController < ApplicationController
   respond_to :json
 
   def index
-    @items = Item.most_revenue(params[:quantity])
+    items = Item.most_revenue(params[:quantity])
 
-    respond_with @items
+    respond_with items
   end
 end
--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Merchants::ItemsController < ApplicationController
   respond_to :json
   def index
-    @items_sold = Merchant.most_items_sold(params[:quantity])
-    respond_with @items_sold
+    items_sold = Merchant.most_items_sold(params[:quantity])
+    respond_with items_sold
   end
 end
