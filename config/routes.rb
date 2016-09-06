feadc6c6d2fc3d39dd2d8800d6c398e68a157c64
Ryan Flach <ryanflach@users.noreply.github.com>
2016-09-06 13:59:40 -0600

    Close #8. Record Endpoint: Invoices - Add routes - Add BaseController for Api/V1 to call respond_to :json - Add responders gem (required for respond_to and respond_with in Rails 5) - Passes tests in spec harness

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      resources :invoices, only: [:show, :index] do
+        get '/find', to: 'invoices/search#show', on: :collection
+        get '/find_all', to: 'invoices/search#index', on: :collection
+        get '/random', to: 'invoices/random#show', on: :collection
+      end
+    end
+  end
 end
