9cdde5665b19abade8c13673f247b81bfa1bb8cb
pwentz <patrick.w.wentz@gmail.com>
2016-09-07 13:25:49 -0600

    close #48 business endpoint all items return top revenue by quantity.

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,18 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+  has_many :transactions, through: :invoices
   default_scope { order(id: :desc) }
 
   def self.most_revenue(quantity)
-    select(
-      "items.*, " \
-      "SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue"
-    ).
+    unscoped.
+      select(
+        "items.*, "\
+        "SUM((invoice_items.unit_price::float / 100) "\
+        "* invoice_items.quantity) AS revenue"
+      ).
+      joins(:transactions).
+      where("transactions.result = 'success'").
       joins(:invoice_items).
       order("revenue DESC").
       group("items.id").
